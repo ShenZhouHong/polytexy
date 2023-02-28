b852a0c297d1f97cc832c3cff5501e7c8ae03d63
Shen Zhou Hong <ShenZhouHong@users.noreply.github.com>
2023-02-28 11:51:36 +0000

    Update pandoc scripts to use defaults file
    As the epub and HTML converters will certainly use more options, I have
    updated the pandoc scripts to take a defaults.yaml file. The
    defaults.yaml file is a configuration file that allows us to specify
    more options for pandoc.

--- pandoc/html/pandoc.sh
+++ pandoc/html/pandoc.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# Markdown to EPUB conversion script    
+# Markdown to HTML conversion script    
 set -eEu -o pipefail
+
+# Retrieve script source directory as DIR. This allows relative pathing to work
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Check if pandoc is available
 if ! command -v pandoc &> /dev/null
@@ -19,29 +22,14 @@ then
     exit 0
 fi
 
-# Retrieve script source directory as DIR. This allows relative pathing to work
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# We run pandoc with the following options. The most important of which are
-#  - Use the basic barebones template.tex template
-#  - Suppress the auto-generation of LaTeX hyperref targets on sections
-#  - Allow raw LaTeX to be passed through from the source markdown file
-#  - Write using a custom LaTeX writer (i.e. pandoc conversion ruleset)
-
 # Run the command multiple times, once for each file to build.
 for file in "$@"
 do
     filename="$(basename $file .md)" # Name of the file without extension
     # $DIR    local path of the script 
     # ${file} local path of the file
-    echo "$DIR"
-    pandoc \
-    "${file}" \
-    --standalone \
-    --wrap=none \
-    --columns=80 \
-    --biblatex \
-    --from markdown-auto_identifiers+raw_tex+citations \
-    --filter "$DIR/../string-substitution.py"  \
+    
+    pandoc "${file}" \
+    --defaults "$DIR/defaults.yaml" \
     --out "$DIR/../../output/html/${filename}".html
 done
--- pandoc/latex/pandoc.sh
+++ pandoc/latex/pandoc.sh
@@ -3,15 +3,18 @@
 # Markdown to LaTeX conversion script    
 set -eEu -o pipefail
 
+# Retrieve script source directory as DIR. This allows relative pathing to work
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Check if pandoc is available
-if ! command -v pandoc &> /dev/null
+if ! command -v pandoc &> /dev/null 
 then
     echo "Error: pandoc is not available. Please install pandoc before running this script."
     exit 1
 fi
 
 # If no arguments were provided or the --help/-h flag was passed, print usage information
-if [[ $# -eq 0 ]] || [[ $1 == "-h" || $1 == "--help" ]]
+if [[ $# -eq 0 ]] || [[ $1 == "-h" || $1 == "--help" ]] 
 then
     echo "Markdown to LaTeX Converter (w/ Pandoc)"
     echo "Usage: $(basename $0) file1.md [file2.md ...]"
@@ -19,31 +22,15 @@ then
     exit 0
 fi
 
-# Retrieve script source directory as DIR. This allows relative pathing to work
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# We run pandoc with the following options. The most important of which are
-#  - Use the basic barebones template.tex template
-#  - Suppress the auto-generation of LaTeX hyperref targets on sections
-#  - Allow raw LaTeX to be passed through from the source markdown file
-#  - Write using a custom LaTeX writer (i.e. pandoc conversion ruleset)
-
 # Run the command multiple times, once for each file to build.
 for file in "$@"
 do
     filename="$(basename $file .md)" # Name of the file without extension
     # $DIR    local path of the script 
     # ${file} local path of the file
-    echo "$DIR"
-    pandoc \
-    "${file}" \
-    --standalone \
-    --wrap=none \
-    --columns=80 \
-    --biblatex \
-    --template="$DIR/latex-base.tex" \
-    --from markdown-auto_identifiers+raw_tex+citations \
-    --to "$DIR/latex-writer.lua" \
-    --filter "$DIR/../string-substitution.py"  \
+    
+    pandoc "${file}" \
+    --defaults "$DIR/defaults.yaml" \
+    --to  "$DIR/latex-writer.lua"  \
     --out "$DIR/../../output/latex/${filename}".tex
 done
